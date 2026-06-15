2b0d7e10fb4941d42f4ee05063d5d4e6b91b7d2a
Walter Schulze <awalterschulze@gmail.com>
2026-06-15 11:43:10 +0100

    update katydid

--- update_katydid.sh
+++ update_katydid.sh
@@ -1,3 +1,4 @@
+set -xe
 (cd implementations/go-katydid-auto-reflect && GOPROXY=direct go get github.com/katydid/validator-go-jsonschema@main && go mod tidy)
 (cd implementations/go-katydid-auto-json && GOPROXY=direct go get github.com/katydid/validator-go-jsonschema@main && go mod tidy)
 (cd implementations/go-katydid-mem-reflect && GOPROXY=direct go get github.com/katydid/validator-go-jsonschema@main && go mod tidy)
