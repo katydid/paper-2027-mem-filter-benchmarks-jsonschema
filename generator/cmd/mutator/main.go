efebd0f1c83e7fb9299181da681725bea519d99c
Walter Schulze <awalterschulze@gmail.com>
2026-06-20 12:12:17 +0100

    let the mutator add fields and see what breaks

--- generator/cmd/mutator/main.go
+++ generator/cmd/mutator/main.go
@@ -22,6 +22,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"slices"
 	"strconv"
 	"strings"
 	"time"
@@ -32,10 +33,21 @@ import (
 	jsonschema "github.com/santhosh-tekuri/jsonschema/v6"
 )
 
+type config struct {
+	addFields bool
+	rmFields  bool
+	addItems  bool
+	rmItems   bool
+}
+
 func main() {
 	log.SetFlags(log.Lshortfile)
 	seed := flag.Int64("seed", time.Now().UnixNano(), "seed for random generator (defaults to now)")
 	num := flag.Int("num", -1, "number of random json lines to generate (defaults to the number of items in the valid instances.jsonl)")
+	addFields := flag.Bool("addFields", true, "when mutating also add consider adding fields to objects")
+	rmFields := flag.Bool("rmFields", false, "when mutating also add consider removing fields from objects")
+	addItems := flag.Bool("addItems", false, "when mutating also add consider adding items to arrays")
+	rmItems := flag.Bool("rmItems", false, "when mutating also add consider removing items from arrays")
 	flag.Parse()
 	args := flag.Args()
 	if len(args) == 0 {
@@ -47,6 +59,12 @@ func main() {
 		dstfolder = strings.Replace(srcfolder, "-valid", "-invalid", 1)
 	}
 	fmt.Printf("mutating %s to %s with seed %d\n", srcfolder, dstfolder, *seed)
+	config := &config{
+		addFields: *addFields,
+		rmFields:  *rmFields,
+		addItems:  *addItems,
+		rmItems:   *rmItems,
+	}
 	r := rand.NewRandWithSeed(*seed)
 	schemaBytes, err := os.ReadFile(filepath.Join(srcfolder, "schema.json"))
 	if err != nil {
@@ -76,7 +94,7 @@ func main() {
 	i := 0
 	for len(newlines) < (*num + 1) {
 		line := lines[i%len(lines)]
-		newline := mutate(r, schema, string(line))
+		newline := mutate(config, r, schema, string(line))
 		if newline == nil {
 			log.Printf("mutation failed for %s", line)
 		} else {
@@ -90,10 +108,10 @@ func main() {
 	}
 }
 
-func mutate(r rand.Rand, v *jsonschema.Schema, line string) *string {
+func mutate(c *config, r rand.Rand, v *jsonschema.Schema, line string) *string {
 	attempts := 100
 	for attempts > 0 {
-		if res := tryFieldMutate(r, v, line); res != nil {
+		if res := tryFieldMutate(c, r, v, line); res != nil {
 			return res
 		}
 		attempts--
@@ -101,17 +119,17 @@ func mutate(r rand.Rand, v *jsonschema.Schema, line string) *string {
 	return nil
 }
 
-func tryFieldMutate(r rand.Rand, v *jsonschema.Schema, line string) *string {
+func tryFieldMutate(c *config, r rand.Rand, v *jsonschema.Schema, line string) *string {
 	m, err := std.UnmarshalToAnyWithJSONNumber([]byte(line))
 	if err != nil {
 		panic(err)
 	}
-	n := numMutationPoints(m)
+	n := numMutationPoints(c, m)
 	if n == 0 {
 		return nil
 	}
 	p := r.Intn(n)
-	m1 := mutatePoint(r, m, &p)
+	m1 := mutatePoint(c, r, m, &p)
 	data, err := json.Marshal(m1)
 	if err != nil {
 		panic(err)
@@ -123,42 +141,78 @@ func tryFieldMutate(r rand.Rand, v *jsonschema.Schema, line string) *string {
 	return try(v, datas)
 }
 
-func mutatePoint(r rand.Rand, a any, p *int) any {
+func randName(r rand.Rand) string {
+	s, err := strconv.Unquote(randjson.String(r, randjson.WithMinStringLength(1)))
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
+
+func randValue(r rand.Rand) any {
+	// TODO: do not generate only ints when more implementations can support more variety
+	// TODO: do not generate just ascii when more implementations can support more variety
+	s := randjson.Value(r, randjson.WithMaxDepth(1), randjson.WithAscii(), randjson.OnlyInts())
+	v, err := std.UnmarshalToAnyWithJSONNumber([]byte(s))
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+func mutatePoint(c *config, r rand.Rand, a any, p *int) any {
 	switch t := a.(type) {
 	case map[string]any:
-		for _, k := range std.SortedKeys(t) {
+		keys := std.SortedKeys(t)
+		for _, k := range keys {
 			v := t[k]
 			if *p == 0 {
-				s, err := strconv.Unquote(randjson.String(r, randjson.WithMinStringLength(1)))
-				if err != nil {
-					panic(err)
-				}
+				s := randName(r)
 				delete(t, k)
 				k = s
 			}
 			*p = *p - 1
-			v = mutatePoint(r, v, p)
+			v = mutatePoint(c, r, v, p)
 			t[k] = v
-			// TODO what about adding or removing fields
+			if c.addFields {
+				if *p == 0 {
+					t[randName(r)] = randValue(r)
+				}
+				*p = *p - 1
+			}
+			if c.rmFields {
+				if *p == 0 {
+					k := keys[r.Intn(len(keys))]
+					delete(t, k)
+				}
+				*p = *p - 1
+			}
 		}
 		return t
 	case []any:
 		for i, v := range t {
-			v = mutatePoint(r, v, p)
+			v = mutatePoint(c, r, v, p)
 			t[i] = v
 			*p = *p - 1
 		}
-		// TODO what about adding or removing items
+		if c.addItems {
+			if *p == 0 {
+				v := randValue(r)
+				t = slices.Insert(t, r.Intn(len(t)+1), v)
+			}
+			*p = *p - 1
+		}
+		if c.rmItems {
+			if *p == 0 {
+				index := r.Intn(len(t))
+				t = slices.Delete(t, index, index)
+			}
+			*p = *p - 1
+		}
 		return t
 	default:
 		if *p == 0 {
-			// TODO: do not generate only ints when more implementations can support more variety
-			// TODO: do not generate just ascii when more implementations can support more variety
-			s := randjson.Value(r, randjson.WithMaxDepth(1), randjson.WithAscii(), randjson.OnlyInts())
-			v, err := std.UnmarshalToAnyWithJSONNumber([]byte(s))
-			if err != nil {
-				panic(err)
-			}
+			v := randValue(r)
 			*p = *p - 1
 			return v
 		}
@@ -167,18 +221,30 @@ func mutatePoint(r rand.Rand, a any, p *int) any {
 	return a
 }
 
-func numMutationPoints(a any) int {
+func numMutationPoints(c *config, a any) int {
 	n := 0
 	switch t := a.(type) {
 	case map[string]any:
 		for _, v := range t {
 			n += 1
-			n += numMutationPoints(v)
+			n += numMutationPoints(c, v)
+		}
+		if c.addFields {
+			n += 1
+		}
+		if c.rmFields && len(t) > 0 {
+			n += 1
 		}
 	case []any:
 		for _, v := range t {
 			n += 1
-			n += numMutationPoints(v)
+			n += numMutationPoints(c, v)
+		}
+		if c.addItems {
+			n += 1
+		}
+		if c.rmItems && len(t) > 0 {
+			n += 1
 		}
 	default:
 		n = 1
